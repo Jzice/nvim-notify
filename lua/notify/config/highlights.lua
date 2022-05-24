6b937f17afe4d9ae3597210b0d40bd548c26bf98
Justice <zhengyi.zhu.hust@gmail.com>
2022-05-24 16:39:25 +0800

    add default ctermfg

--- lua/notify/config/highlights.lua
+++ lua/notify/config/highlights.lua
@@ -7,14 +7,14 @@ function M.setup()
     hi default NotifyINFOBorder guifg=#4F6752
     hi default NotifyDEBUGBorder guifg=#8B8B8B
     hi default NotifyTRACEBorder guifg=#4F3552
-    hi default NotifyERRORIcon guifg=#F70067
-    hi default NotifyWARNIcon guifg=#F79000
-    hi default NotifyINFOIcon guifg=#A9FF68
+    hi default NotifyERRORIcon ctermfg=DarkRed guifg=#F70067
+    hi default NotifyWARNIcon ctermfg=DarkYellow guifg=#F79000
+    hi default NotifyINFOIcon ctermfg=DarkGreen guifg=#A9FF68
     hi default NotifyDEBUGIcon guifg=#8B8B8B
     hi default NotifyTRACEIcon guifg=#D484FF
-    hi default NotifyERRORTitle  guifg=#F70067
-    hi default NotifyWARNTitle guifg=#F79000
-    hi default NotifyINFOTitle guifg=#A9FF68
+    hi default NotifyERRORTitle ctermfg=DarkRed guifg=#F70067
+    hi default NotifyWARNTitle ctermfg=DarkYellow guifg=#F79000
+    hi default NotifyINFOTitle ctermfg=DarkGreen guifg=#A9FF68
     hi default NotifyDEBUGTitle  guifg=#8B8B8B
     hi default NotifyTRACETitle  guifg=#D484FF
     hi default link NotifyERRORBody Normal
